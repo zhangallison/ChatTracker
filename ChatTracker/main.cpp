38f18c6b7ed241a101785146a74c90f044e76dc2
zhangallison <alzhang19@ucla.edu>
2020-10-16 15:08:36 -0700

    Initial Commit

---
+++ ChatTracker/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ChatTracker
+//
+//  Created by Allison Zhang on 10/16/20.
+//  Copyright © 2020 Allison Zhang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
